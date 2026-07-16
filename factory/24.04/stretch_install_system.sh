86d8126a5a7d848404184158a8100396eace8cfe
Bharadwaj Chukkala <bharadwaj@hello-robot.com>
2026-07-15 18:07:26 -0700

    update ros2 installation command (#28)
    Co-authored-by: production <production@hello-robot.com>

--- factory/24.04/stretch_install_system.sh
+++ factory/24.04/stretch_install_system.sh
@@ -82,9 +82,9 @@ echo "INSTALLATION OF ROS 2 JAZZY"
 echo "###########################################"
 echo "Install ros-apt-source"
 function install_ros_apt_source {
-    export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+    export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
     echo $ROS_APT_SOURCE_VERSION
-    curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
+    curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
     sudo dpkg -i /tmp/ros2-apt-source.deb
 }
 install_ros_apt_source &>> $REDIRECT_LOGFILE
